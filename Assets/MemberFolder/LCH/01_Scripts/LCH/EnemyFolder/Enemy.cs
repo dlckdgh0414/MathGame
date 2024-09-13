0d744cde47052494d0dd8bddf2337996d0c32569
dlckdgh0414 <ci5976547@gmail.com>
2024-09-13 23:03:17 +0900

    fix ?: Error

--- Assets/MemberFolder/LCH/01_Scripts/LCH/EnemyFolder/Enemy.cs
+++ Assets/MemberFolder/LCH/01_Scripts/LCH/EnemyFolder/Enemy.cs
@@ -20,7 +20,6 @@ public class Enemy : EnemySetting
     public float distance;
     public bool FirstWake = true;
     public bool fainting = false;
-    public Pooling _enemyPooling;
     public StateMachine<EnemyStateEnum> StateMachine { get; set; }
 
     public void GetHit()
@@ -28,54 +27,12 @@ public class Enemy : EnemySetting
         StateMachine.ChangeState(EnemyStateEnum.Hit);
     }
 
-    public override void SetDeadState()
-    {
-        StateMachine.ChangeState(EnemyStateEnum.Dead);
-    }
-    protected override void Awake()
-    {
-        if(!IsDie)
-        StateMachine = new StateMachine<EnemyStateEnum>();
-        base.Awake();
-    }
-
-    protected virtual void Start()
-    {
-        StateMachine.InitInitialize(EnemyStateEnum.Idle, this);
-        StartCoroutine(Delaytime());
-    }
-
-    protected virtual void Update()
-    {
-        StateMachine.CurrentState.UpdateState();
-        if (GetPlayer())
-        {
-            MovementComponent._xMove = 0F;
-        }
-    }
-
-    private void LateUpdate()
-    {
-        StateMachine.CurrentState.LateUpdateState();
-    }
+    //public override void SetDeadState()
+    //{
+    //    StateMachine.ChangeState(EnemyStateEnum.Dead);
+    //}
     public override void AnimationEndTrigger()
     {
         StateMachine.CurrentState.AnimationEndTrigger();
     }
-
-    protected IEnumerator Delaytime()
-    {
-        if (!IsDie)
-        {
-            while (true)
-            {
-                dir = GetRandomVector() - transform.position;
-                yield return new WaitForSeconds(2f);
-                MovementComponent._xMove = 0F;
-                yield return new WaitForSeconds(2f);
-            }
-        }
-
-    }
-
 }
