23015c5b34d6651c638c3bebacd1bd7c3c840806
dlckdgh0414 <ci5976547@gmail.com>
2024-09-15 15:34:30 +0900

    feat : DoTween, ITrunalbe, EnemyStats

---
+++ Assets/MemberFolder/LCH/01_Scripts/LCH/EnemyFolder/EnemyStatsSo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName =("So/Stats"))]
+public class EnemyStatsSo : ScriptableObject
+{
+	public int Hp = 0;
+}
