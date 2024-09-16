604e00d138a81f05b0415b43345377d82703ef17
dlckdgh0414 <ci5976547@gmail.com>
2024-09-16 21:49:50 +0900

    feat : Exel

---
+++ Assets/ExcelImporter/Example/Scripts/Example.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
+
+public class Example : MonoBehaviour
+{
+	[SerializeField] MstItems mstItems;
+	[SerializeField] TextMeshProUGUI _text;
+
+	void Start()
+	{
+		ShowItems();
+	}
+
+	void ShowItems()
+	{
+		string str = "";
+
+		mstItems.Entities
+			.ForEach(entity => str += DescribeMstItemEntity(entity) + "\n");
+
+		_text.text = str;
+	}
+
+	string DescribeMstItemEntity(MstItemEntity entity)
+	{
+		return string.Format(
+			"{0} : {1}, {2}, {3}, {4}, {5}",
+			entity.id,
+			entity.name,
+			entity.price,
+			entity.isNotForSale,
+			entity.rate,
+			entity.category
+		);
+	}
+}
+
---
+++ Assets/MemberFolder/LCH/Lch/ProblemFilesDB.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class ProblemFilesDB : MonoBehaviour
+{
+	public string Problem;
+
+	public string Choice1;
+
+	public string Choice2;
+
+	public string Choice3;
+
+	public string Choice4;
+
+	public int ResultNumber;
+}
