765afad539a57d8fb357387c34f5c8b0f6c61f4a
dlckdgh0414 <ci5976547@gmail.com>
2024-09-16 18:31:29 +0900

    fix : Error

--- Assets/MemberFolder/LCH/Lch/ProblemJsonLoad.cs
+++ Assets/MemberFolder/LCH/Lch/ProblemJsonLoad.cs
@@ -12,11 +12,9 @@ public class ProblemJsonLoad : MonoBehaviour
 
     void Start()
     {
-        sourcePath = Application.persistentDataPath + "/questions.json";
+        sourcePath = Application.dataPath + "/questions.json";
         targetPath = Application.dataPath + "/questions.json";
         Debug.Log("StreamingAssets Path: " + Application.streamingAssetsPath);
-        // 게임 실행 파일이 있는 경로에 JSON 파일 저장
-        // 파일이 있으면 내용을 불러옴
         Debug.Log("JSON 파일 저장 경로: " + targetPath); Debug.Log("Source Path: " + sourcePath);
         Debug.Log("Target Path: " + targetPath);
         LoadJSONFile();
@@ -30,9 +28,6 @@ public class ProblemJsonLoad : MonoBehaviour
             // 파일에서 JSON 데이터를 읽어옴
             string jsonData = File.ReadAllText(targetPath);
             questionList = JsonUtility.FromJson<QuestionList>(jsonData);
-
-            // 첫 번째 문제 출력
-            Debug.Log("첫 번째 문제: " + questionList.questions[0].question);
         }
         else
         {
