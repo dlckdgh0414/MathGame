0c97d48ae36384d0f8d1334a8be489abbc952de1
rlaals0203 <scientistmin@gmail.com>
2024-09-16 13:32:51 +0900

    perf : GameManager

---
+++ Assets/MemberFolder/LCH/01_Scripts/LCH/EnemyFolder/EnemyStatSOList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/EnemyStatSO")]
+public class EnemyStatSOList : ScriptableObject
+{
+    public List<EnemyStatsSo> enemyStatList;
+}
