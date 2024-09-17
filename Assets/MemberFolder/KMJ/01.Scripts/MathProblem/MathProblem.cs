28766522eca3b11b6091d203489af3de906fb20a
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-09-17 23:54:18 +0900

    fix - mathProblem

--- Assets/MemberFolder/KMJ/01.Scripts/MathProblem/MathProblem.cs
+++ Assets/MemberFolder/KMJ/01.Scripts/MathProblem/MathProblem.cs
@@ -6,24 +6,34 @@ using UnityEngine.UI;
 
 public class MathProblem : MonoBehaviour
 {
-    public List<TextMeshProUGUI> problemAnswer = new List<TextMeshProUGUI>();
-    public List<Button> _problemButton = new List<Button>();
+    public ProblemFile problemfile;
 
+    public List<TextMeshProUGUI> ProblemText;
+    public TextMeshProUGUI problemName;
+
+    public static int ProblemNumber = 0;
     private int _randomInt;
 
-
+    private void OnEnable()
+    {
+        _randomInt = Random.Range(0, 5);
+        Debug.Log(_randomInt);
+    }
     private void Awake()
     {
-        _randomInt = Random.Range(0,5);
+        
     }
     private void Start()
     {
-        
     }
 
 
     private void Update()
-    {
-
+    {      
+        problemName.text = problemfile.Problems[ProblemNumber].Problem;
+        ProblemText[0].text = problemfile.Problems[ProblemNumber].Choice1;
+        ProblemText[1].text = problemfile.Problems[ProblemNumber].Choice2;
+        ProblemText[2].text = problemfile.Problems[ProblemNumber].Choice3;
+        ProblemText[3].text = problemfile.Problems[ProblemNumber].Choice4;
     }
 }
