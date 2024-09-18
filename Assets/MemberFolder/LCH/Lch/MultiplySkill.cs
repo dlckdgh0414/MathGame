d7abf7dc8da2fb91c3e23ef4b23f419e252c532a
dlckdgh0414 <ci5976547@gmail.com>
2024-09-19 00:48:12 +0900

    feat : MulitplySkill

---
+++ Assets/MemberFolder/LCH/Lch/MultiplySkill.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MultiplySkill : MonoBehaviour
+{
+	[SerializeField] private float minY, maxY;
+	[SerializeField] private float minX, maxX;
+
+    [SerializeField] private int Count = 0;
+
+    public Pooling enemy;
+
+    private void Start()
+    { 
+
+    }
+
+    public void Pase1()
+    {
+        float rangY = Random.Range(minY, maxY);
+        float rangX = Random.Range(minX, maxX);
+
+        enemy = PoolManager.Instance.Pop("MultiplyAttack") as Pooling;
+        if (enemy != null)
+        {
+            enemy.gameObject.transform.position = new Vector2();
+        }
+    }
+}
