4cd567be5a9f49c7105a6588ceb3411ac266451a
dlckdgh0414 <ci5976547@gmail.com>
2024-09-23 23:14:26 +0900

    fix : Erorr

---
+++ Assets/MemberFolder/KMJ/LCH/Lch/FourBasicOperationsSkil.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FourBasicOperationsSkil : MonoBehaviour
+{
+    private Enemy _enmyOwer;
+    private void Awake()
+    {
+        _enmyOwer = GetComponentInParent<Enemy>();
+    }
+    public void Pases()
+    {
+        if(_enmyOwer.HelathCompo.Hp > 90)
+        {
+            Pase1();
+        }
+        else if(_enmyOwer.HelathCompo.Hp <= 90)
+        {
+            Pase2();
+        } 
+        else if(_enmyOwer.HelathCompo.Hp <= 5)
+        {
+            LastPase();
+        }
+    }
+
+
+    private void Pase1()
+    {
+        int rand = Random.Range(0,4);
+
+        switch (rand)
+        {
+            case 1:
+                StartCoroutine(Pase1Attack1());
+                break;
+            case 2:
+                StartCoroutine(Pase1Attack2());
+                break;
+        }
+    }
+
+    private IEnumerator Pase1Attack2()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            PoolManager.Instance.Pop("PlusAttack3");
+        }
+    }
+
+    private IEnumerator Pase1Attack1()
+    {
+        PoolManager.Instance.Pop("PlusAttackBase");
+        yield return new WaitForSeconds(0.5f);
+        Ipoolable item = GameObject.Find("PlusAttack2").GetComponent<Ipoolable>();
+        PoolManager.Instance.Push(item);
+
+    }
+
+    private void Pase2()
+    {
+        
+    }
+    private void LastPase()
+    {
+        
+    }
+
+}
