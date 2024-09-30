3b180b8deb4d26406d388904200e31172be32b05
rlaals0203 <scientistmin@gmail.com>
2024-09-30 23:40:57 +0900

    feat : Dictionary

---
+++ Assets/MemberFolder/KM/01_Script/DictionaryOpen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class DictionaryOpen : MonoBehaviour
+{
+    [SerializeField] private GameObject _dic;
+    [SerializeField] private GameObject _player;
+    private GameObject _enemy;
+
+    private bool isOpne = false;
+
+    public void OnDictionaryClick()
+    {
+        _enemy = GameObject.FindWithTag("Enemy");
+        isOpne = !isOpne;
+
+        _player.gameObject.GetComponent<SpriteRenderer>().enabled = !isOpne;
+        _enemy.gameObject.GetComponent<SpriteRenderer>().enabled = !isOpne;
+        _dic.SetActive(isOpne);
+    }
+}
