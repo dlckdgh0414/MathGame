492b97626f2329fda38fe8579ffea3fe319c54a0
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-09-30 23:16:01 +0900

    .

--- Assets/MemberFolder/LCH/Lch/EnemyAttack.cs
+++ Assets/MemberFolder/LCH/Lch/EnemyAttack.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class EnemyAttack : MonoBehaviour
 {
 	private Health _playerHealth;
     private Enemy _enemy;
+    private AudioSource _hitSound;
+
+    private void Awake()
+    {
+        _hitSound = GameObject.Find("PigutSound").GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            _hitSound.Play();
             Debug.Log("АјАн");
             _playerHealth = GameObject.Find("Player").GetComponent<Health>();
             _enemy = GameObject.FindWithTag("Enemy").GetComponent<Enemy>();
