0e4ead5a94b20ee1e1e619bb594b579168032f4d
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2024-09-30 22:32:10 +0900

    .

--- Assets/MemberFolder/KM/01_Script/Combat/DivisionAttack.cs
+++ Assets/MemberFolder/KM/01_Script/Combat/DivisionAttack.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class DivisionAttack : MonoBehaviour
 {
